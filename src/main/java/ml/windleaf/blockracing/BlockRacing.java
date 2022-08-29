8c6120dbeb3917a4c54a033a24c4e3e5ca26a514
WindLeaf233 <mc.windleaf@foxmail.com>
2022-08-29 19:46:18 +0800

    :tada: begin project

---
+++ src/main/java/ml/windleaf/blockracing/BlockRacing.java
@@ -0,0 +1,17 @@
+package ml.windleaf.blockracing;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlockRacing extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
