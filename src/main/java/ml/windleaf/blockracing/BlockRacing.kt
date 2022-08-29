be8278dab9df155830bcad00c7076610e6e0b32f
WindLeaf233 <mc.windleaf@foxmail.com>
2022-08-29 19:55:41 +0800

    :heavy_plus_sign: add kotlin dependency

---
+++ src/main/java/ml/windleaf/blockracing/BlockRacing.kt
@@ -0,0 +1,17 @@
+package ml.windleaf.blockracing
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class BlockRacing: JavaPlugin() {
+  override fun onLoad() {
+    super.onLoad()
+  }
+
+  override fun onEnable() {
+    super.onEnable()
+  }
+
+  override fun onDisable() {
+    super.onDisable()
+  }
+}
