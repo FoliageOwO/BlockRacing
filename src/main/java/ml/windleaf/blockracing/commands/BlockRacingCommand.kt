0c0075448ad6edc7acfb1b1237dc1be601eeccd8
WindLeaf233 <mc.windleaf@foxmail.com>
2022-09-17 17:56:44 +0800

    :sparkles: implement basic game logic

--- src/main/java/ml/windleaf/blockracing/commands/BlockRacingCommand.kt
+++ src/main/java/ml/windleaf/blockracing/commands/BlockRacingCommand.kt
@@ -4,15 +4,20 @@ import ml.windleaf.blockracing.BlockRacing
 import ml.windleaf.blockracing.BlockRacing.Companion.game
 import ml.windleaf.blockracing.BlockRacing.Companion.pluginLogger
 import ml.windleaf.blockracing.configurations.GoalsConfig
+import ml.windleaf.blockracing.configurations.PluginConfig
 import ml.windleaf.blockracing.entity.goals.GoalColumn
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.command.TabCompleter
+import kotlin.properties.Delegates
 
 class BlockRacingCommand: CommandExecutor, TabCompleter {
   private val config = BlockRacing.configInstances["goals"] as GoalsConfig
+  private val pluginConfig = BlockRacing.configInstances["config"] as PluginConfig
   private lateinit var sender: CommandSender
+  private var score by Delegates.notNull<Int>()
+  private var round by Delegates.notNull<Int>()
 
   override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
     this.sender = sender
@@ -23,17 +28,20 @@ class BlockRacingCommand: CommandExecutor, TabCompleter {
         when (first) {
           "help" -> getHelp()
           "goals" -> getGoals()
+          "start" -> startGame(score, round)
           else -> errorCommand()
         }
       }
       2 -> {
         when (args[0]) {
-          "start" -> {
-            try {
-              val score = args[1].toInt()
-              startGame(score)
-            } catch (e: NumberFormatException) { errorArgs() }
-          }
+          "score" -> try { score(args[1].toInt()) } catch (e: NumberFormatException) { errorArgs() }
+          "round" -> try { round(args[1].toInt()) } catch (e: NumberFormatException) { errorArgs() }
+          else -> errorCommand()
+        }
+      }
+      3 -> {
+        when (args[0]) {
+          "start" -> try { startGame(args[1].toInt(), args[2].toInt()) } catch (e: NumberFormatException) { errorArgs() }
           else -> errorCommand()
         }
       }
@@ -53,12 +61,21 @@ class BlockRacingCommand: CommandExecutor, TabCompleter {
       "&a--- [BlockRacing Help] ---",
       "&2/br [help] &f- &6查看此帮助",
       "&2/br goals &f- &6查看所有可用目标",
-      "&2/br start <score> &f- &6自定义目标分数开始游戏"
+      "&2/br start &f- &6使用已有设置开始游戏",
+      "&2/br start <score> <round> &f- &6自定义目标分数和轮数开始游戏",
+      "&2/br score <value> &f- &6自定义游戏分数",
+      "&2/br round <value> &f- &6自定义游戏轮数"
     ).forEach {
       pluginLogger.send(sender, it, withPrefix = false)
     }
 
-  private fun startGame(score: Int) = game.start(score)
+  private fun score(score: Int) { this.score = score }
+
+  private fun round(round: Int) { this.round = round }
+
+  private fun startGame(score: Int?, round: Int?) =
+    game.start(score ?: pluginConfig.get("game.default-score") as Int,
+      round ?: pluginConfig.get("game.default-round") as Int)
 
   private fun getGoals() {
     val goals = config.getGoals()
--- src/main/java/ml/windleaf/blockracing/game/Game.kt
+++ src/main/java/ml/windleaf/blockracing/game/Game.kt
@@ -1,7 +1,57 @@
 package ml.windleaf.blockracing.game
 
+import ml.windleaf.blockracing.BlockRacing
+import ml.windleaf.blockracing.BlockRacing.Companion.pluginLogger
+import ml.windleaf.blockracing.BlockRacing.Companion.scoreboardManager
+import ml.windleaf.blockracing.BlockRacing.Companion.teamManager
+import ml.windleaf.blockracing.configurations.GoalsConfig
+import ml.windleaf.blockracing.configurations.PluginConfig
+import ml.windleaf.blockracing.entity.goals.GoalBlock
+import ml.windleaf.blockracing.entity.goals.Rating
+import ml.windleaf.blockracing.team.Team
+
 class Game {
-  fun start(score: Int) {
-    // todo
+  private val pluginConfig = BlockRacing.configInstances["config"] as PluginConfig
+  private val goals = BlockRacing.configInstances["goals"] as GoalsConfig
+  private var score: Int = 0
+  private var round: Int = 0
+  private val unique = pluginConfig.get("game.unique-goals") as Boolean
+
+  fun start(score: Int, round: Int) {
+    this.score = score
+    this.round = round
+
+    log("&f检测队伍...")
+    val teams = teamManager.getTeams()
+    if (teams.size <= 1) {
+      log("&e未检测到队伍, 开始随机分配队伍...")
+      teamManager.randomizePlayers(pluginConfig.get("game.default-team-size") as Int)
+    }
+
+    log("&f分配队伍目标...")
+    var availableGoals = arrayListOf<GoalBlock>()
+    this.goals.getGoals().forEach { column ->
+      column.blocks.forEach { block -> availableGoals.add(block) }
+    }
+    val goals: HashMap<Team, ArrayList<GoalBlock>> = hashMapOf()
+    teams.forEach { team ->
+      availableGoals = ArrayList(availableGoals.shuffled())
+      val g = if (availableGoals.size <= round) availableGoals else availableGoals.subList(0, round - 1)
+      goals[team] = ArrayList(g)
+      if (!unique) availableGoals.removeAll(g.toSet())
+    }
+
+    log("&f配置计分板...")
+    teams.forEach { team ->
+      scoreboardManager.updateGoals(team, goals[team]!!)
+      team.players.values.forEach { player ->
+        val t = team.info
+        pluginLogger.send(player, "&a游戏即将开始, 你的队伍是: ${t.color}${t.teamName}&a!")
+        scoreboardManager.newBoard(player)
+      }
+    }
+    scoreboardManager.render()
   }
+
+  private fun log(msg: String) = pluginLogger.log(msg)
 }
